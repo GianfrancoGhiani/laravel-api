c33f5c0014847dbe126549ab136b2bfef42c8d31
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-01-17 16:04:54 +0100

    api created

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+
+class ProjectController extends Controller
+
+
+{
+    public function index()
+    {
+        $projects = Project::with('technologies', 'tag')->get();
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+    public function show($slug)
+    {
+        $project = Project::with('technologies', 'tag')->where('slug', $slug)->first();
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Nessun project trovato'
+            ]);
+        }
+    }
+}
